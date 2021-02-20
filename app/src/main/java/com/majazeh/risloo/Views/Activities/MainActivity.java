aa3a39fc86970656e5e59601144f59351621cb9f
Amir Dehbashi <xamirdehbashix@gmail.com>
2021-02-20 19:23:00 +0330

    added menu and bell images to main

--- app/src/main/java/com/majazeh/risloo/Views/Activities/MainActivity.java
+++ app/src/main/java/com/majazeh/risloo/Views/Activities/MainActivity.java
@@ -1,12 +1,16 @@
 package com.majazeh.risloo.Views.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.content.res.AppCompatResources;
+import androidx.core.view.GravityCompat;
+import androidx.core.widget.ImageViewCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.ImageView;
 
 import com.google.android.material.navigation.NavigationView;
 import com.majazeh.risloo.R;
@@ -20,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
     // Widgets
     private DrawerLayout drawerLayout;
     private NavigationView navigationView;
+    private ImageView toolbarMenuImageView, toolbarBellImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,16 +56,36 @@ public class MainActivity extends AppCompatActivity {
         drawerLayout = findViewById(R.id.activity_main);
 
         navigationView = findViewById(R.id.activity_main_navigationView);
+
+        toolbarMenuImageView = findViewById(R.id.activity_main_content_menu_imageView);
+        toolbarMenuImageView.setImageResource(R.drawable.ic_bars_light);
+        ImageViewCompat.setImageTintList(toolbarMenuImageView, AppCompatResources.getColorStateList(this, R.color.Gray500));
+        toolbarBellImageView = findViewById(R.id.activity_main_content_bell_imageView);
+        toolbarBellImageView.setImageResource(R.drawable.ic_bell_light);
+        ImageViewCompat.setImageTintList(toolbarBellImageView, AppCompatResources.getColorStateList(this, R.color.Gray500));
     }
 
     private void detector() {
         if (Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP) {
-            // TODO : Place Code Here
+            toolbarMenuImageView.setBackgroundResource(R.drawable.draw_2sdp_solid_white_border_gray300_ripple_gray400);
+            toolbarBellImageView.setBackgroundResource(R.drawable.draw_2sdp_solid_white_border_gray300_ripple_gray400);
         }
     }
 
     private void listener() {
-        // TODO : Place Code Here
+        toolbarMenuImageView.setOnClickListener(v -> {
+            toolbarMenuImageView.setClickable(false);
+            handler.postDelayed(() -> toolbarMenuImageView.setClickable(true), 300);
+
+            drawerLayout.openDrawer(GravityCompat.START);
+        });
+
+        toolbarBellImageView.setOnClickListener(v -> {
+            toolbarBellImageView.setClickable(false);
+            handler.postDelayed(() -> toolbarBellImageView.setClickable(true), 300);
+
+
+        });
     }
 
     private void setData() {
