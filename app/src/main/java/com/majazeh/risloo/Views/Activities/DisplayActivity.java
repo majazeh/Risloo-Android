8bd13832fc30a920cce144727b4e4ef0ff71d679
Amir Dehbashi <xamirdehbashix@gmail.com>
2021-02-20 16:24:37 +0330

    added some stuffs 4

--- app/src/main/java/com/majazeh/risloo/Views/Activities/DisplayActivity.java
+++ app/src/main/java/com/majazeh/risloo/Views/Activities/DisplayActivity.java
@@ -2,6 +2,8 @@ package com.majazeh.risloo.Views.Activities;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.content.res.AppCompatResources;
+import androidx.core.widget.ImageViewCompat;
 
 import android.content.pm.PackageManager;
 import android.os.Build;
@@ -9,7 +11,6 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
 import android.widget.ImageView;
-import android.widget.TextView;
 
 import com.jsibbold.zoomage.ZoomageView;
 import com.majazeh.risloo.R;
@@ -22,116 +23,117 @@ import com.squareup.picasso.Picasso;
 
 public class DisplayActivity extends AppCompatActivity {
 
-//    // Vars
-//    private String title = "", bitmap = "", path = "";
-//
-//    // Objects
-//    private Bundle extras;
-//    private Handler handler;
-//
-//    // Widgets
-//    private ImageView toolbarBackImageView, toolbarDownloadImageView;
-//    private TextView toolbarTextView;
-//    private ZoomageView avatarZoomageView;
+    // Vars
+    private String title = "", bitmap = "", path = "";
+
+    // Objects
+    private Bundle extras;
+    private Handler handler;
+
+    // Widgets
+    private ImageView toolbarReturnImageView, toolbarDownloadImageView;
+    private ZoomageView avatarZoomageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-//        decorator();
+        decorator();
 
         setContentView(R.layout.activity_display);
 
-//        initializer();
-//
-//        detector();
-//
-//        listener();
-//
-//        setData();
+        initializer();
+
+        detector();
+
+        listener();
+
+        setData();
     }
 
-//    private void decorator() {
-//        WindowDecorator windowDecorator = new WindowDecorator();
-//
-//        windowDecorator.darkShowSystemUI(this);
-//        windowDecorator.darkSetSystemUIColor(this, getResources().getColor(R.color.Gray900), getResources().getColor(R.color.Gray900));
-//    }
-//
-//    private void initializer() {
-//        extras = getIntent().getExtras();
-//
-//        handler = new Handler();
-//
-//        toolbarTextView = findViewById(R.id.component_toolbar_textView);
-//
-//        toolbarBackImageView = findViewById(R.id.component_toolbar_primary_imageView);
-//        toolbarDownloadImageView = findViewById(R.id.component_toolbar_secondary_imageView);
-//
-//        avatarZoomageView = findViewById(R.id.activity_display_avatar_zoomageView);
-//    }
-//
-//    private void detector() {
-//        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP) {
-//            toolbarBackImageView.setBackgroundResource(R.drawable.draw_oval_solid_gray900_ripple_white);
-//            toolbarDownloadImageView.setBackgroundResource(R.drawable.draw_oval_solid_gray900_ripple_white);
-//        }
-//    }
-//
-//    private void listener() {
-//        toolbarBackImageView.setOnClickListener(v -> {
-//            toolbarBackImageView.setClickable(false);
-//            handler.postDelayed(() -> toolbarBackImageView.setClickable(true), 250);
-//
-//            finish();
-//        });
-//
-//        toolbarDownloadImageView.setOnClickListener(v -> {
-//            toolbarDownloadImageView.setClickable(false);
-//            handler.postDelayed(() -> toolbarDownloadImageView.setClickable(true), 250);
-//
-//            if (PermissionManager.storagePermission(this)) {
-//                IntentManager.download(this, bitmap);
-//            }
-//        });
-//    }
-//
-//    private void setData() {
-//        if (extras.getString("title") != null) {
-//            title = extras.getString("title");
-//        }
-//        if (extras.getString("bitmap") != null) {
-//            bitmap = extras.getString("bitmap");
-//        }
-//        if (extras.getString("path") != null) {
-//            path = extras.getString("path");
-//        }
-//
-//        if (!title.equals("")) {
-//            toolbarTextView.setText(title);
-//        }
-//        if (!bitmap.equals("")) {
-//            Picasso.get().load(bitmap).placeholder(R.color.Gray900).into(avatarZoomageView);
-//            toolbarDownloadImageView.setVisibility(View.VISIBLE);
-//        }
-//        if (!path.equals("")) {
-//            avatarZoomageView.setImageBitmap(BitmapManager.modifyOrientation(FileManager.readBitmapFromCache(this, "bitmap"), path));
-//            FileManager.deleteFileFromCache(this, "bitmap");
-//        }
-//    }
-//
-//    @Override
-//    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-//        if (requestCode == 200) {
-//            if (grantResults.length > 0) {
-//                for (int grantResult : grantResults) {
-//                    if (grantResult != PackageManager.PERMISSION_GRANTED) {
-//                        return;
-//                    }
-//                }
-//                IntentManager.download(this, bitmap);
-//            }
-//        }
-//    }
+    private void decorator() {
+        WindowDecorator windowDecorator = new WindowDecorator();
+
+        windowDecorator.darkShowSystemUI(this);
+        windowDecorator.darkSetSystemUIColor(this, getResources().getColor(R.color.Gray900), getResources().getColor(R.color.Gray900));
+    }
+
+    private void initializer() {
+        extras = getIntent().getExtras();
+
+        handler = new Handler();
+
+        toolbarReturnImageView = findViewById(R.id.activity_display_return_imageView);
+        toolbarReturnImageView.setImageResource(R.drawable.ic_angle_light);
+        ImageViewCompat.setImageTintList(toolbarReturnImageView, AppCompatResources.getColorStateList(this, R.color.Gray50));
+        toolbarDownloadImageView = findViewById(R.id.activity_display_download_imageView);
+        toolbarDownloadImageView.setImageResource(R.drawable.ic_download_light);
+        ImageViewCompat.setImageTintList(toolbarDownloadImageView, AppCompatResources.getColorStateList(this, R.color.Gray50));
+
+        avatarZoomageView = findViewById(R.id.activity_display_avatar_zoomageView);
+    }
+
+    private void detector() {
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP) {
+            toolbarReturnImageView.setBackgroundResource(R.drawable.draw_2sdp_solid_gray900_border_gray200_ripple_gray300);
+            toolbarDownloadImageView.setBackgroundResource(R.drawable.draw_2sdp_solid_gray900_border_gray200_ripple_gray300);
+        }
+    }
+
+    private void listener() {
+        toolbarReturnImageView.setOnClickListener(v -> {
+            toolbarReturnImageView.setClickable(false);
+            handler.postDelayed(() -> toolbarReturnImageView.setClickable(true), 300);
+
+            finish();
+        });
+
+        toolbarDownloadImageView.setOnClickListener(v -> {
+            toolbarDownloadImageView.setClickable(false);
+            handler.postDelayed(() -> toolbarDownloadImageView.setClickable(true), 300);
+
+            if (PermissionManager.storagePermission(this)) {
+                IntentManager.download(this, bitmap);
+            }
+        });
+    }
+
+    private void setData() {
+        if (extras.getString("title") != null) {
+            title = extras.getString("title");
+        }
+        if (extras.getString("bitmap") != null) {
+            bitmap = extras.getString("bitmap");
+        }
+        if (extras.getString("path") != null) {
+            path = extras.getString("path");
+        }
+
+        if (!title.equals("")) {
+            // TODO ; Place Code If Needed
+        }
+        if (!bitmap.equals("")) {
+            Picasso.get().load(bitmap).placeholder(R.color.Gray900).into(avatarZoomageView);
+            toolbarDownloadImageView.setVisibility(View.VISIBLE);
+        }
+        if (!path.equals("")) {
+            avatarZoomageView.setImageBitmap(BitmapManager.modifyOrientation(FileManager.readBitmapFromCache(this, "bitmap"), path));
+            FileManager.deleteFileFromCache(this, "bitmap");
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == 200) {
+            if (grantResults.length > 0) {
+                for (int grantResult : grantResults) {
+                    if (grantResult != PackageManager.PERMISSION_GRANTED) {
+                        return;
+                    }
+                }
+                IntentManager.download(this, bitmap);
+            }
+        }
+    }
 
 }
--- app/src/main/java/com/majazeh/risloo/Views/Activities/MainActivity.java
+++ app/src/main/java/com/majazeh/risloo/Views/Activities/MainActivity.java
@@ -2,16 +2,59 @@ package com.majazeh.risloo.Views.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
 
 import com.majazeh.risloo.R;
+import com.majazeh.risloo.Utils.Managers.WindowDecorator;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Objects
+    private Handler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        decorator();
+
         setContentView(R.layout.activity_main);
+
+        initializer();
+
+        detector();
+
+        listener();
+
+        setData();
+    }
+
+    private void decorator() {
+        WindowDecorator windowDecorator = new WindowDecorator();
+
+        windowDecorator.lightNavShowSystemUI(this);
+        windowDecorator.lightSetSystemUIColor(this, Color.TRANSPARENT, getResources().getColor(R.color.Gray50));
+    }
+
+    private void initializer() {
+        handler = new Handler();
+    }
+
+    private void detector() {
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP) {
+            // TODO : Place Code Here
+        }
+    }
+
+    private void listener() {
+        // TODO : Place Code Here
+    }
+
+    private void setData() {
+        // TODO : Place Code Here
     }
 
 }
