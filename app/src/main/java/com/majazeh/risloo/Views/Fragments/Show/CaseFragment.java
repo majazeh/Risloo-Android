89fe8da1a4c3fed24951bd0248a0ff449191a452
Amir Dehbashi <xamirdehbashix@gmail.com>
2021-02-24 13:17:44 +0330

    added new fragments

---
+++ app/src/main/java/com/majazeh/risloo/Views/Fragments/Show/CaseFragment.java
@@ -0,0 +1,30 @@
+package com.majazeh.risloo.Views.Fragments.Show;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.majazeh.risloo.R;
+
+public class CaseFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup viewGroup,  @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_case, viewGroup, false);
+
+        initializer(view);
+
+        return view;
+    }
+
+    private void initializer(View view) {
+
+    }
+
+}
