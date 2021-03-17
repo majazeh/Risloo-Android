0a959bfee6c5ccc5fc2793a2314f47c5b244c9ac
Amir Dehbashi <xamirdehbashix@gmail.com>
2021-03-17 19:18:59 +0330

    session halfway

---
+++ app/src/main/java/com/majazeh/risloo/Views/Adapters/Recycler/PsychologistsAdapter.java
@@ -0,0 +1,90 @@
+package com.majazeh.risloo.Views.Adapters.Recycler;
+
+import android.app.Activity;
+import android.os.Build;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.majazeh.risloo.R;
+import com.majazeh.risloo.Utils.Managers.StringManager;
+import com.majazeh.risloo.databinding.SingleItemPsychologyBinding;
+import com.squareup.picasso.Picasso;
+
+public class PsychologistsAdapter extends RecyclerView.Adapter<PsychologistsAdapter.PsychologistsHolder> {
+
+    // Objects
+    private Activity activity;
+
+    // Vars
+//    private ArrayList<Psychology> psychologists;
+
+    public PsychologistsAdapter(@NonNull Activity activity) {
+        this.activity = activity;
+    }
+
+    @NonNull
+    @Override
+    public PsychologistsHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        return new PsychologistsHolder(SingleItemPsychologyBinding.inflate(LayoutInflater.from(activity), viewGroup, false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull PsychologistsHolder holder, int i) {
+//        Psychologists psychology = psychologists.get(i);
+
+        detector(holder);
+
+        listener(holder);
+
+        setData(holder);
+    }
+
+    @Override
+    public int getItemCount() {
+//        return psychologists.size();
+        return 15;
+    }
+
+//    public void setPsychology(ArrayList<Psychology> psychologists) {
+//        this.psychologists = psychologists;
+//        notifyDataSetChanged();
+//    }
+
+    private void detector(PsychologistsHolder holder) {
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP) {
+            holder.binding.containerConstraintLayout.setBackgroundResource(R.drawable.draw_2sdp_solid_white_border_1sdp_gray200_ripple_gray300);
+        }
+    }
+
+    private void listener(PsychologistsHolder holder) {
+        holder.binding.containerConstraintLayout.setOnClickListener(v -> {
+            holder.binding.containerConstraintLayout.setClickable(false);
+
+            // TODO : Place Code Here
+        });
+    }
+
+    private void setData(PsychologistsHolder holder) {
+        holder.binding.nameTextView.setText("ریلسو");
+
+        holder.binding.avatarIncludeLayout.charTextView.setVisibility(View.VISIBLE);
+        holder.binding.avatarIncludeLayout.charTextView.setText(StringManager.firstChars(holder.binding.nameTextView.getText().toString()));
+
+        Picasso.get().load(R.color.Gray50).placeholder(R.color.Gray50).into(holder.binding.avatarIncludeLayout.avatarCircleImageView);
+    }
+
+    public class PsychologistsHolder extends RecyclerView.ViewHolder {
+
+        private SingleItemPsychologyBinding binding;
+
+        public PsychologistsHolder(SingleItemPsychologyBinding binding) {
+            super(binding.getRoot());
+            this.binding = binding;
+        }
+    }
+
+}
---
+++ app/src/main/java/com/majazeh/risloo/Views/Adapters/Recycler/ReferencesAdapter.java
@@ -0,0 +1,82 @@
+package com.majazeh.risloo.Views.Adapters.Recycler;
+
+import android.app.Activity;
+import android.os.Build;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.majazeh.risloo.R;
+import com.majazeh.risloo.databinding.SingleItemReferenceBinding;
+
+public class ReferencesAdapter extends RecyclerView.Adapter<ReferencesAdapter.ReferencesHolder> {
+
+    // Objects
+    private Activity activity;
+
+    // Vars
+//    private ArrayList<Reference> references;
+
+    public ReferencesAdapter(@NonNull Activity activity) {
+        this.activity = activity;
+    }
+
+    @NonNull
+    @Override
+    public ReferencesHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        return new ReferencesHolder(SingleItemReferenceBinding.inflate(LayoutInflater.from(activity), viewGroup, false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ReferencesHolder holder, int i) {
+//        References Reference = references.get(i);
+
+        detector(holder);
+
+        listener(holder);
+
+        setData(holder);
+    }
+
+    @Override
+    public int getItemCount() {
+//        return references.size();
+        return 15;
+    }
+
+//    public void setReference(ArrayList<Reference> references) {
+//        this.references = references;
+//        notifyDataSetChanged();
+//    }
+
+    private void detector(ReferencesHolder holder) {
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP) {
+            holder.binding.containerConstraintLayout.setBackgroundResource(R.drawable.draw_2sdp_solid_white_border_1sdp_gray200_ripple_gray300);
+        }
+    }
+
+    private void listener(ReferencesHolder holder) {
+        holder.binding.containerConstraintLayout.setOnClickListener(v -> {
+            holder.binding.containerConstraintLayout.setClickable(false);
+
+            // TODO : Place Code Here
+        });
+    }
+
+    private void setData(ReferencesHolder holder) {
+        holder.binding.nameTextView.setText("ریلسو");
+    }
+
+    public class ReferencesHolder extends RecyclerView.ViewHolder {
+
+        private SingleItemReferenceBinding binding;
+
+        public ReferencesHolder(SingleItemReferenceBinding binding) {
+            super(binding.getRoot());
+            this.binding = binding;
+        }
+    }
+
+}
