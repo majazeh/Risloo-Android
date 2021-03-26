4fa4884d3e39f7411444ef9031fc58373e7942ac
Amir Dehbashi <xamirdehbashix@gmail.com>
2021-03-26 18:37:13 +0430

    create xml files finished

---
+++ app/src/main/java/com/majazeh/risloo/Views/Fragments/Create/CreateRoomUserFragment.java
@@ -0,0 +1,33 @@
+package com.majazeh.risloo.Views.Fragments.Create;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.majazeh.risloo.databinding.FragmentCreateRoomUserBinding;
+
+public class CreateRoomUserFragment extends Fragment {
+
+    // Binding
+    private FragmentCreateRoomUserBinding binding;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup viewGroup,  @Nullable Bundle savedInstanceState) {
+        binding = FragmentCreateRoomUserBinding.inflate(inflater, viewGroup, false);
+
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+}
