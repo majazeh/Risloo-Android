13e3bc870de600bf4846ae3d975450b5b00eb4ec
Amir Dehbashi <xamirdehbashix@gmail.com>
2021-04-19 03:09:03 +0430

    added test bulk fragment

---
+++ app/src/main/java/com/majazeh/risloo/Views/Adapters/Recycler/BulksAdapter.java
@@ -0,0 +1,89 @@
+package com.majazeh.risloo.Views.Adapters.Recycler;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.content.res.AppCompatResources;
+import androidx.core.content.res.ResourcesCompat;
+import androidx.core.widget.ImageViewCompat;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.majazeh.risloo.R;
+import com.majazeh.risloo.databinding.SingleItemBulkBinding;
+
+public class BulksAdapter extends RecyclerView.Adapter<BulksAdapter.BulksHolder> {
+
+    // Objects
+    private Activity activity;
+
+    // Vars
+//    private ArrayList<Bulk> bulks;
+
+    public BulksAdapter(@NonNull Activity activity) {
+        this.activity = activity;
+    }
+
+    @NonNull
+    @Override
+    public BulksHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        return new BulksHolder(SingleItemBulkBinding.inflate(LayoutInflater.from(activity), viewGroup, false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull BulksHolder holder, int i) {
+//        Bulks bulk = bulks.get(i);
+
+        setData(holder);
+    }
+
+    @Override
+    public int getItemCount() {
+//        return bulks.size();
+        return 5;
+    }
+
+//    public void setBulk(ArrayList<Bulk> bulks) {
+//        this.bulks = bulks;
+//        notifyDataSetChanged();
+//    }
+
+    private void setData(BulksHolder holder) {
+        if (holder.getAdapterPosition() == 0) {
+            holder.binding.topView.setVisibility(View.GONE);
+        } else {
+            holder.binding.topView.setVisibility(View.VISIBLE);
+        }
+
+        holder.binding.nameTextView.setText("آزمون سلامت عمومی (1)");
+
+        if (holder.getAdapterPosition() == 0) {
+            holder.binding.nameTextView.setTextColor(activity.getResources().getColor(R.color.Blue700));
+            holder.binding.nameTextView.setTextAppearance(activity, R.style.danaDemiBoldTextStyle);
+
+            holder.binding.activeImageView.setBackgroundResource(0);
+            holder.binding.activeImageView.setImageDrawable(ResourcesCompat.getDrawable(activity.getResources(), R.drawable.ic_chevron_circle_left_solid, null));
+            ImageViewCompat.setImageTintList(holder.binding.activeImageView, AppCompatResources.getColorStateList(activity, R.color.Blue600));
+        } else {
+            holder.binding.nameTextView.setTextColor(activity.getResources().getColor(R.color.Gray500));
+            holder.binding.nameTextView.setTextAppearance(activity, R.style.danaMediumTextStyle);
+
+            holder.binding.activeImageView.setBackgroundResource(R.drawable.draw_oval_solid_transparent_border_1sdp_gray500);
+            holder.binding.activeImageView.setImageDrawable(null);
+            ImageViewCompat.setImageTintList(holder.binding.activeImageView, null);
+        }
+    }
+
+    public class BulksHolder extends RecyclerView.ViewHolder {
+
+        private SingleItemBulkBinding binding;
+
+        public BulksHolder(SingleItemBulkBinding binding) {
+            super(binding.getRoot());
+            this.binding = binding;
+        }
+    }
+
+}
---
+++ app/src/main/java/com/majazeh/risloo/Views/Fragments/Test/TestBulkFragment.java
@@ -0,0 +1,65 @@
+package com.majazeh.risloo.Views.Fragments.Test;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.majazeh.risloo.R;
+import com.majazeh.risloo.Utils.Managers.InitManager;
+import com.majazeh.risloo.Utils.Widgets.ItemDecorateRecyclerView;
+import com.majazeh.risloo.Views.Adapters.Recycler.BulksAdapter;
+import com.majazeh.risloo.databinding.FragmentTestBulkBinding;
+
+public class TestBulkFragment extends Fragment {
+
+    // Binding
+    private FragmentTestBulkBinding binding;
+
+    // Adapters
+    private BulksAdapter bulksAdapter;
+
+    // Objects
+    private RecyclerView.ItemDecoration itemDecoration;
+    private LinearLayoutManager layoutManager;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup viewGroup,  @Nullable Bundle savedInstanceState) {
+        binding = FragmentTestBulkBinding.inflate(inflater, viewGroup, false);
+
+        initializer();
+
+        setData();
+
+        return binding.getRoot();
+    }
+
+    private void initializer() {
+        bulksAdapter = new BulksAdapter(requireActivity());
+
+        itemDecoration = new ItemDecorateRecyclerView("verticalLayout", (int) getResources().getDimension(R.dimen._16sdp), (int) getResources().getDimension(R.dimen._12sdp), 0, (int) getResources().getDimension(R.dimen._12sdp));
+
+        layoutManager = new LinearLayoutManager(requireActivity(), LinearLayoutManager.VERTICAL, false);
+
+        InitManager.recyclerView(binding.bulksSingleLayout.recyclerView, itemDecoration, layoutManager);
+    }
+
+    private void setData() {
+//        bulksAdapter.setBulk(null);
+        binding.bulksSingleLayout.recyclerView.setAdapter(bulksAdapter);
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+}
