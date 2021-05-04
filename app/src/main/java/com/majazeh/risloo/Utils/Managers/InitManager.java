d5831a5dd3a5a2fdea6ea13de2df8aed2eb51dd5
Amir Dehbashi <xamirdehbashix@gmail.com>
2021-05-04 05:05:39 +0430

    changed toolbar fourth dropdown item text color in dashboard

--- app/src/main/java/com/majazeh/risloo/Utils/Managers/InitManager.java
+++ app/src/main/java/com/majazeh/risloo/Utils/Managers/InitManager.java
@@ -1,6 +1,8 @@
 package com.majazeh.risloo.Utils.Managers;
 
 import android.app.Activity;
+import android.os.Build;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -8,6 +10,8 @@ import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.content.res.AppCompatResources;
 import androidx.core.widget.ImageViewCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -61,10 +65,48 @@ public class InitManager {
                 Collections.addAll(list, activity.getResources().getStringArray(arrayRes));
                 ArrayAdapter<String> adapter = new ArrayAdapter<String>(activity, R.layout.spinner_item_background_toolbar, list) {
 
+                    private TextView dropdownTextView;
+
                     @Override
-                    public View getView(int position, View convertView, ViewGroup viewGroup) {
-                        View view = super.getView(position, convertView, viewGroup);
+                    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup viewGroup) {
+                        return super.getView(position, convertView, viewGroup);
+                    }
+
+                    @Override
+                    public View getDropDownView(int position, @Nullable View convertView, @NonNull ViewGroup viewGroup) {
+                        View view = LayoutInflater.from(activity).inflate(R.layout.spinner_item_dropdown_toolbar, viewGroup, false);
+
+                        initializer(view);
+
+                        detector(view, position);
+
+                        setData(position);
+
                         return view;
+                    }
+
+                    private void initializer(View view) {
+                        dropdownTextView = view.findViewById(R.id.textView);
+                    }
+
+                    private void detector(View view, int position) {
+                        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP) {
+                            if (position == 4) {
+                                view.setBackgroundResource(R.drawable.draw_rec_solid_gray50_ripple_red300);
+                            } else {
+                                view.setBackgroundResource(R.drawable.draw_rec_solid_gray50_ripple_gray300);
+                            }
+                        }
+                    }
+
+                    private void setData(int position) {
+                        dropdownTextView.setText(list.get(position));
+
+                        if (position == 4) {
+                            dropdownTextView.setTextColor(activity.getResources().getColor(R.color.Red500));
+                        } else {
+                            dropdownTextView.setTextColor(activity.getResources().getColor(R.color.Gray600));
+                        }
                     }
 
                     @Override
@@ -73,7 +115,7 @@ public class InitManager {
                     }
 
                 };
-                adapter.setDropDownViewResource(R.layout.spinner_item_dropdown_toolbar);
+
                 spinner.setAdapter(adapter);
                 spinner.setSelection(adapter.getCount());
                 break;
