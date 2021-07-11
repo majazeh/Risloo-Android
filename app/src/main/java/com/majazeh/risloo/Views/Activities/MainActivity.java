06a0a798b0bb93c825791dfc5cbf628a1a9e5f8b
Amir Dehbashi <xamirdehbashix@gmail.com>
2021-07-11 20:00:29 +0430

    bread cump idea created and tested
    part 2

--- app/src/main/java/com/majazeh/risloo/Views/Activities/MainActivity.java
+++ app/src/main/java/com/majazeh/risloo/Views/Activities/MainActivity.java
@@ -202,7 +202,7 @@ public class MainActivity extends AppCompatActivity {
         });
 
         navController.addOnDestinationChangedListener((controller, destination, arguments) -> {
-            binding.contentIncludeLayout.breadcumpTextView.setText(breadCrumb.getBreadCrumbFa(destination, arguments));
+            binding.contentIncludeLayout.breadcumpTextView.setText(breadCrumb.getFa(destination, arguments));
             binding.contentIncludeLayout.breadcumpTextView.setMovementMethod(LinkMovementMethod.getInstance());
         });
     }
