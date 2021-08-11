efc584e45ec9fa7ab0f25b16bccc7d5204dcc7b9
Amir Dehbashi <xamirdehbashix@gmail.com>
2021-08-11 18:54:31 +0430

    added some permissions

--- app/src/main/java/com/majazeh/risloo/Utils/Entities/Permissoon.java
+++ app/src/main/java/com/majazeh/risloo/Utils/Entities/Permissoon.java
@@ -1,5 +1,6 @@
 package com.majazeh.risloo.Utils.Entities;
 
+import com.mre.ligheh.Model.TypeModel.AcceptationModel;
 import com.mre.ligheh.Model.TypeModel.UserModel;
 
 public class Permissoon {
@@ -9,23 +10,38 @@ public class Permissoon {
     */
 
     public static boolean showEditUserTabPersonalMobile(UserModel model) {
-        return model.getUserType().equals("admin");
+        if (model != null)
+            return model.getUserType().equals("admin");
+        else
+            return false;
     }
 
     public static boolean showEditUserTabPersonalEmail(UserModel model) {
-        return model.getUserType().equals("admin");
+        if (model != null)
+            return model.getUserType().equals("admin");
+        else
+            return false;
     }
 
     public static boolean showEditUserTabPersonalBirthday(UserModel model) {
-        return model.getUserType().equals("admin");
+        if (model != null)
+            return model.getUserType().equals("admin");
+        else
+            return false;
     }
 
     public static boolean showEditUserTabPersonalStatus(UserModel model) {
-        return model.getUserType().equals("admin");
+        if (model != null)
+            return model.getUserType().equals("admin");
+        else
+            return false;
     }
 
     public static boolean showEditUserTabPersonalType(UserModel model) {
-        return model.getUserType().equals("admin");
+        if (model != null)
+            return model.getUserType().equals("admin");
+        else
+            return false;
     }
 
     /*
@@ -33,7 +49,92 @@ public class Permissoon {
     */
 
     public static boolean showEditUserTabPasswordCurrent(UserModel model) {
-        return !model.getUserType().equals("admin") && !model.isNo_password();
+        if (model != null)
+            return !model.getUserType().equals("admin") && !model.isNo_password();
+        else
+            return false;
+    }
+
+    /*
+    ---------- Centers ----------
+    */
+
+    public static boolean showCentersCreateCenter(UserModel model) {
+        if (model != null)
+            return model.getUserType().equals("admin");
+        else
+            return false;
+    }
+
+    /*
+    ---------- Center ----------
+    */
+
+    public static boolean showCenterDropdownUsers(AcceptationModel model) {
+        if (model != null)
+            return model.getPosition().equals("admin") || model.getPosition().equals("manager") || model.getPosition().equals("operator");
+        else
+            return false;
+    }
+
+    public static boolean showCenterDropdownEdit(AcceptationModel model) {
+        if (model != null)
+            return model.getPosition().equals("admin") || model.getPosition().equals("manager") || model.getPosition().equals("operator");
+        else
+            return false;
+    }
+
+    public static boolean showCenterDropdownPlatforms(AcceptationModel model) {
+        if (model != null)
+            return model.getPosition().equals("admin") || model.getPosition().equals("manager") || model.getPosition().equals("operator");
+        else
+            return false;
+    }
+
+    public static boolean showCenterDropdownTags(AcceptationModel model) {
+        if (model != null)
+            return model.getPosition().equals("admin") || model.getPosition().equals("manager") || model.getPosition().equals("operator");
+        else
+            return false;
+    }
+
+    public static boolean showCenterCreateRoom(AcceptationModel model) {
+        if (model != null)
+            return model.getPosition().equals("admin") || model.getPosition().equals("manager");
+        else
+            return false;
+    }
+
+    /*
+    ---------- Room ----------
+    */
+
+    public static boolean showRoomDropdownCreateSchedule(AcceptationModel model) {
+        if (model != null)
+            return model.getPosition().equals("admin") || model.getPosition().equals("manager") || model.getPosition().equals("operator");
+        else
+            return false;
+    }
+
+    public static boolean showRoomDropdownUsers(AcceptationModel model) {
+        if (model != null)
+            return model.getPosition().equals("admin") || model.getPosition().equals("manager") || model.getPosition().equals("operator");
+        else
+            return false;
+    }
+
+    public static boolean showRoomDropdownPlatforms(AcceptationModel model) {
+        if (model != null)
+            return model.getPosition().equals("admin") || model.getPosition().equals("manager") || model.getPosition().equals("operator");
+        else
+            return false;
+    }
+
+    public static boolean showRoomDropdownTags(AcceptationModel model) {
+        if (model != null)
+            return model.getPosition().equals("admin") || model.getPosition().equals("manager") || model.getPosition().equals("operator");
+        else
+            return false;
     }
 
 }
--- app/src/main/java/com/majazeh/risloo/Views/Fragments/Index/CentersFragment.java
+++ app/src/main/java/com/majazeh/risloo/Views/Fragments/Index/CentersFragment.java
@@ -19,6 +19,7 @@ import androidx.navigation.NavDirections;
 
 import com.majazeh.risloo.NavigationMainDirections;
 import com.majazeh.risloo.R;
+import com.majazeh.risloo.Utils.Entities.Permissoon;
 import com.majazeh.risloo.Utils.Managers.ClickManager;
 import com.majazeh.risloo.Utils.Managers.InitManager;
 import com.majazeh.risloo.Utils.Managers.StringManager;
@@ -28,6 +29,7 @@ import com.majazeh.risloo.databinding.FragmentCentersBinding;
 import com.mre.ligheh.API.Response;
 import com.mre.ligheh.Model.Madule.Center;
 import com.mre.ligheh.Model.Madule.List;
+import com.mre.ligheh.Model.TypeModel.UserModel;
 
 import java.util.HashMap;
 import java.util.Objects;
@@ -57,6 +59,8 @@ public class CentersFragment extends Fragment {
         detector();
 
         listener();
+
+        setPermission();
 
         getData();
 
@@ -142,6 +146,15 @@ public class CentersFragment extends Fragment {
             NavDirections action = NavigationMainDirections.actionGlobalCreateCenterFragment(null, null);
             ((MainActivity) requireActivity()).navController.navigate(action);
         }).widget(binding.addImageView.getRoot());
+    }
+
+    private void setPermission() {
+        UserModel model = ((MainActivity) requireActivity()).singleton.getUserModel();
+
+        if (Permissoon.showCentersCreateCenter(model))
+            binding.addImageView.getRoot().setVisibility(View.VISIBLE);
+        else
+            binding.addImageView.getRoot().setVisibility(View.GONE);
     }
 
     private void getData() {
