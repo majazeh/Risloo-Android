4903747c346801963896e812c91bff72db87f0f3
Amir Dehbashi <xamirdehbashix@gmail.com>
2021-08-21 19:44:46 +0430

    cleaned string

--- app/src/main/java/com/majazeh/risloo/Utils/Managers/StringManager.java
+++ app/src/main/java/com/majazeh/risloo/Utils/Managers/StringManager.java
@@ -13,6 +13,10 @@ import java.text.DecimalFormat;
 
 public class StringManager {
 
+    /*
+    ---------- Logics ----------
+    */
+
     public static String bracing(int value) {
         return "(" + value + ")";
     }
@@ -25,17 +29,12 @@ public class StringManager {
         return value.substring(value.length() - index);
     }
 
-    public static String replace(String value, String oldText, String newText) {
-        return value.replace(oldText, newText);
-    }
-
     public static String substring(String value, char character) {
         int position = 0;
 
         for (int i = 0; i < value.length(); i++) {
-            if (value.charAt(i) == character) {
+            if (value.charAt(i) == character)
                 position = i;
-            }
         }
 
         return value.substring(position + 1);
@@ -46,32 +45,21 @@ public class StringManager {
     }
 
     public static String firstChars(String value) {
-        String cc = "";
-
-        if (!cc.equals(value)) {
-            if (value.contains(" ")) {
-                cc = value.charAt(0) + String.valueOf(value.substring(value.lastIndexOf(" ") + 1).charAt(0));
-            } else if (value.contains("-")) {
-                cc = value.charAt(0) + String.valueOf(value.substring(value.lastIndexOf("-") + 1).charAt(0));
-            } else if (value.contains("/")) {
-                cc = value.charAt(0) + String.valueOf(value.substring(value.lastIndexOf("/") + 1).charAt(0));
-            } else {
-                cc = value.charAt(0) + String.valueOf(value.charAt(1));
-            }
-        }
-
-        return cc;
+        if (!value.equals("")) {
+            if (value.contains(" "))
+                return value.charAt(0) + String.valueOf(value.substring(value.lastIndexOf(" ") + 1).charAt(0));
+            else if (value.contains("-"))
+                return value.charAt(0) + String.valueOf(value.substring(value.lastIndexOf("-") + 1).charAt(0));
+            else if (value.contains("/"))
+                return value.charAt(0) + String.valueOf(value.substring(value.lastIndexOf("/") + 1).charAt(0));
+            else
+                return value.charAt(0) + String.valueOf(value.charAt(1));
+        } return value;
     }
 
     public static String separate(String value) {
         DecimalFormat decimalFormat = new DecimalFormat("#,###,###");
         return decimalFormat.format(Double.parseDouble(value));
-    }
-
-    public static SpannableString lining(String value) {
-        SpannableString spannableString = new SpannableString(value);
-        spannableString.setSpan(new StrikethroughSpan(), 0, value.length(), Spanned.SPAN_MARK_MARK);
-        return spannableString;
     }
 
     public static String persian(String value) {
@@ -80,94 +68,29 @@ public class StringManager {
         if (value.length() == 0) {
             return "";
         } else {
-            for (int i = 0; i < value.length(); i++) {
-                char c = value.charAt(i);
-                if ('0' <= c && c <= '9') {
-                    output.append(persianNumbers[Integer.parseInt(String.valueOf(c))]);
-                } else if (c == '.' || c == ',' || c == 'و') {
+            for (char ch: value.toCharArray()) {
+                if ('0' <= ch && ch <= '9')
+                    output.append(persianNumbers[Integer.parseInt(String.valueOf(ch))]);
+                else if (ch == '.' || ch == ',' || ch == 'و')
                     output.append(",");
-                } else {
-                    output.append(c);
-                }
-            }
-            return output.toString();
+                else
+                    output.append(ch);
+            } return output.toString();
         }
     }
 
-    public static SpannableString separateLining(String value) {
-        DecimalFormat decimalFormat = new DecimalFormat("#,###,###");
-        String decimalFormatString = decimalFormat.format(Double.parseDouble(value));
-        SpannableString spannableString = new SpannableString(decimalFormatString);
-        spannableString.setSpan(new StrikethroughSpan(), 0, decimalFormatString.length(), Spanned.SPAN_MARK_MARK);
+    public static String replace(String value, String oldText, String newText) {
+        return value.replace(oldText, newText);
+    }
+
+    /*
+    ---------- Customize One ----------
+    */
+
+    public static SpannableString strikethrough(String value, int startIndex, int endIndex) {
+        SpannableString spannableString = new SpannableString(value);
+        spannableString.setSpan(new StrikethroughSpan(), startIndex, endIndex, Spanned.SPAN_MARK_MARK);
         return spannableString;
-    }
-
-    public static String separatePersian(String value) {
-        DecimalFormat decimalFormat = new DecimalFormat("#,###,###");
-        String decimalFormatString = decimalFormat.format(Double.parseDouble(value));
-        String[] persianNumbers = new String[]{"۰", "۱", "۲", "۳", "۴", "۵", "۶", "۷", "'۸", "۹"};
-        StringBuilder output = new StringBuilder();
-        if (decimalFormatString.length() == 0) {
-            return "";
-        } else {
-            for (int i = 0; i < decimalFormatString.length(); i++) {
-                char c = decimalFormatString.charAt(i);
-                if ('0' <= c && c <= '9') {
-                    output.append(persianNumbers[Integer.parseInt(String.valueOf(c))]);
-                } else if (c == '.' || c == ',' || c == 'و') {
-                    output.append(",");
-                } else {
-                    output.append(c);
-                }
-            }
-            return output.toString();
-        }
-    }
-
-    public static SpannableString persianLining(String value) {
-        String[] persianNumbers = new String[]{"۰", "۱", "۲", "۳", "۴", "۵", "۶", "۷", "'۸", "۹"};
-        StringBuilder output = new StringBuilder();
-        if (value.length() == 0) {
-            return null;
-        } else {
-            for (int i = 0; i < value.length(); i++) {
-                char c = value.charAt(i);
-                if ('0' <= c && c <= '9') {
-                    output.append(persianNumbers[Integer.parseInt(String.valueOf(c))]);
-                } else if (c == '.' || c == ',' || c == 'و') {
-                    output.append(",");
-                } else {
-                    output.append(c);
-                }
-            }
-            SpannableString spannableString = new SpannableString(output.toString());
-            spannableString.setSpan(new StrikethroughSpan(), 0, output.length(), Spanned.SPAN_MARK_MARK);
-            return spannableString;
-        }
-    }
-
-    public static SpannableString separatePersianLining(String value) {
-        DecimalFormat decimalFormat = new DecimalFormat("#,###,###");
-        String decimalFormatString = decimalFormat.format(Double.parseDouble(value));
-        String[] persianNumbers = new String[]{"۰", "۱", "۲", "۳", "۴", "۵", "۶", "۷", "'۸", "۹"};
-        StringBuilder output = new StringBuilder();
-        if (decimalFormatString.length() == 0) {
-            return null;
-        } else {
-            for (int i = 0; i < decimalFormatString.length(); i++) {
-                char c = decimalFormatString.charAt(i);
-                if ('0' <= c && c <= '9') {
-                    output.append(persianNumbers[Integer.parseInt(String.valueOf(c))]);
-                } else if (c == '.' || c == ',' || c == 'و') {
-                    output.append(",");
-                } else {
-                    output.append(c);
-                }
-            }
-            SpannableString spannableString = new SpannableString(output.toString());
-            spannableString.setSpan(new StrikethroughSpan(), 0, output.length(), Spanned.SPAN_MARK_MARK);
-            return spannableString;
-        }
     }
 
     public static SpannableString clickable(String value, int startIndex, int endIndex, ClickableSpan clickableSpan) {
@@ -204,12 +127,33 @@ public class StringManager {
         return spannableString;
     }
 
+    /*
+    ---------- Customize Two ----------
+    */
+
+    public static SpannableString separateStrike(String value, int startIndex, int endIndex) {
+        DecimalFormat decimalFormat = new DecimalFormat("#,###,###");
+        String decimalFormatString = decimalFormat.format(Double.parseDouble(value));
+        SpannableString spannableString = new SpannableString(decimalFormatString);
+        spannableString.setSpan(new StrikethroughSpan(), startIndex, endIndex, Spanned.SPAN_MARK_MARK);
+        return spannableString;
+    }
+
     public static SpannableString foregroundBackground(String value, int startIndex, int endIndex, int foregroundColor, int backgroundColor) {
         SpannableString spannableString = new SpannableString(value);
         ForegroundColorSpan foregroundColorSpan = new ForegroundColorSpan(foregroundColor);
         BackgroundColorSpan backgroundColorSpan = new BackgroundColorSpan(backgroundColor);
         spannableString.setSpan(foregroundColorSpan, startIndex, endIndex, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
         spannableString.setSpan(backgroundColorSpan, startIndex, endIndex, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        return spannableString;
+    }
+
+    public static SpannableString backgroundSize(String value, int startIndex, int endIndex, int backgroundColor, int size) {
+        SpannableString spannableString = new SpannableString(value);
+        BackgroundColorSpan backgroundColorSpan = new BackgroundColorSpan(backgroundColor);
+        AbsoluteSizeSpan sizeSpan = new AbsoluteSizeSpan(size);
+        spannableString.setSpan(backgroundColorSpan, startIndex, endIndex, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        spannableString.setSpan(sizeSpan, startIndex, endIndex, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
         return spannableString;
     }
 
@@ -222,12 +166,27 @@ public class StringManager {
         return spannableString;
     }
 
+    /*
+    ---------- Customize Three ----------
+    */
+
     public static SpannableString foregroundSizeStyle(String value, int startIndex, int endIndex, int foregroundColor, int size, int typeface) {
         SpannableString spannableString = new SpannableString(value);
         ForegroundColorSpan foregroundColorSpan = new ForegroundColorSpan(foregroundColor);
         AbsoluteSizeSpan sizeSpan = new AbsoluteSizeSpan(size);
         StyleSpan styleSpan = new StyleSpan(typeface);
         spannableString.setSpan(foregroundColorSpan, startIndex, endIndex, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        spannableString.setSpan(sizeSpan, startIndex, endIndex, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        spannableString.setSpan(styleSpan, startIndex, endIndex, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        return spannableString;
+    }
+
+    public static SpannableString backgroundSizeStyle(String value, int startIndex, int endIndex, int backgroundColor, int size, int typeface) {
+        SpannableString spannableString = new SpannableString(value);
+        BackgroundColorSpan backgroundColorSpan = new BackgroundColorSpan(backgroundColor);
+        AbsoluteSizeSpan sizeSpan = new AbsoluteSizeSpan(size);
+        StyleSpan styleSpan = new StyleSpan(typeface);
+        spannableString.setSpan(backgroundColorSpan, startIndex, endIndex, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
         spannableString.setSpan(sizeSpan, startIndex, endIndex, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
         spannableString.setSpan(styleSpan, startIndex, endIndex, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
         return spannableString;
