544670784c0358bf23e62d7f4956f3a08f7ce93f
Amir Dehbashi <xamirdehbashix@gmail.com>
2021-09-02 02:22:19 +0430

    redesigned session show
    part 3

--- app/src/main/java/com/majazeh/risloo/Views/Adapters/Recycler/SchedulesAdapter.java
+++ app/src/main/java/com/majazeh/risloo/Views/Adapters/Recycler/SchedulesAdapter.java
@@ -19,7 +19,10 @@ import com.majazeh.risloo.Views.Adapters.Holder.SchedulesHolder;
 import com.majazeh.risloo.databinding.SingleItemScheduleBinding;
 import com.mre.ligheh.Model.TypeModel.ScheduleModel;
 import com.mre.ligheh.Model.TypeModel.TypeModel;
+import com.mre.ligheh.Model.TypeModel.UserModel;
 import com.squareup.picasso.Picasso;
+
+import org.json.JSONException;
 
 import java.util.ArrayList;
 
@@ -107,21 +110,55 @@ public class SchedulesAdapter extends RecyclerView.Adapter<SchedulesHolder> {
     }
 
     private void setData(SchedulesHolder holder, ScheduleModel model) {
-        holder.binding.timeTextView.setText("ساعت" + " " + DateManager.jalHHsMM(String.valueOf(model.getStarted_at())));
-        holder.binding.durationTextView.setText(model.getDuration() + " دقیقه");
-        holder.binding.nameTextView.setText(model.getRoom().getRoomManager().getName());
+        try {
+            holder.binding.timeTextView.setText("ساعت" + " " + DateManager.jalHHsMM(String.valueOf(model.getStarted_at())));
+            holder.binding.durationTextView.setText(model.getDuration() + " دقیقه");
+            holder.binding.nameTextView.setText(model.getRoom().getRoomManager().getName());
 
-        if (model.isGroup_session())
-            holder.binding.bulkTextView.setVisibility(View.VISIBLE);
-        else
-            holder.binding.bulkTextView.setVisibility(View.GONE);
+            if (model.getClients() != null && model.getClients().data().size() != 0) {
+                holder.binding.referenceGroup.setVisibility(View.VISIBLE);
+                holder.binding.referenceTextView.setText("");
+                for (int i = 0; i < model.getClients().data().size(); i++) {
+                    UserModel user = (UserModel) model.getClients().data().get(i);
+                    if (user != null) {
+                        holder.binding.referenceTextView.append(user.getName());
+                        if (i != model.getClients().data().size() - 1) {
+                            holder.binding.referenceTextView.append("  -  ");
+                        }
+                    }
+                }
+            } else {
+                holder.binding.referenceGroup.setVisibility(View.GONE);
+            }
 
-        setStatus(holder, model.getStatus());
+            if (model.getFields() != null && model.getFields().length() != 0) {
+                holder.binding.axisGroup.setVisibility(View.VISIBLE);
+                holder.binding.axisTextView.setText("");
+                for (int i = 0; i < model.getFields().length(); i++) {
+                    holder.binding.axisTextView.append(model.getFields().getJSONObject(i).getString("title"));
+                    if (i != model.getFields().length() - 1) {
+                        holder.binding.axisTextView.append("  |  ");
+                    }
+                }
+            } else {
+                holder.binding.axisGroup.setVisibility(View.GONE);
+            }
 
-        if (model.getRoom() != null && model.getRoom().getRoomManager() != null && model.getRoom().getRoomManager().getAvatar() != null && model.getRoom().getRoomManager().getAvatar().getMedium() != null)
-            setAvatar(holder, model.getRoom().getRoomManager().getAvatar().getMedium().getUrl());
-        else
-            setAvatar(holder, "");
+            if (model.isGroup_session())
+                holder.binding.bulkTextView.setVisibility(View.VISIBLE);
+            else
+                holder.binding.bulkTextView.setVisibility(View.GONE);
+
+            setStatus(holder, model.getStatus());
+
+            if (model.getRoom() != null && model.getRoom().getRoomManager() != null && model.getRoom().getRoomManager().getAvatar() != null && model.getRoom().getRoomManager().getAvatar().getMedium() != null)
+                setAvatar(holder, model.getRoom().getRoomManager().getAvatar().getMedium().getUrl());
+            else
+                setAvatar(holder, "");
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 
     private void setStatus(SchedulesHolder holder, String status) {
