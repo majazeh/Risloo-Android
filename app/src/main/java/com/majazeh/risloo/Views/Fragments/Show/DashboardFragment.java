520fee048ec96dd097a8046f5ba040cea8eccb57
Amir Dehbashi <xamirdehbashix@gmail.com>
2021-09-05 17:03:06 +0430

    redesigned dashboard
    part 1

--- app/src/main/java/com/majazeh/risloo/Views/Fragments/Show/DashboardFragment.java
+++ app/src/main/java/com/majazeh/risloo/Views/Fragments/Show/DashboardFragment.java
@@ -13,10 +13,7 @@ import com.majazeh.risloo.R;
 import com.majazeh.risloo.Utils.Managers.InitManager;
 import com.majazeh.risloo.Utils.Managers.StringManager;
 import com.majazeh.risloo.Views.Activities.MainActivity;
-import com.majazeh.risloo.Views.Adapters.Recycler.Cases2Adapter;
-import com.majazeh.risloo.Views.Adapters.Recycler.CentersAdapter;
 import com.majazeh.risloo.Views.Adapters.Recycler.RoomsAdapter;
-import com.majazeh.risloo.Views.Adapters.Recycler.Index.IndexSampleAdapter;
 import com.majazeh.risloo.databinding.FragmentDashboardBinding;
 import com.mre.ligheh.API.Response;
 import com.mre.ligheh.Model.Madule.User;
@@ -30,10 +27,7 @@ public class DashboardFragment extends Fragment {
     private FragmentDashboardBinding binding;
 
     // Adapters
-    private Cases2Adapter cases2Adapter;
-    private IndexSampleAdapter indexSampleAdapter;
     private RoomsAdapter roomsAdapter;
-    private CentersAdapter centersAdapter;
 
     // Objects
     private HashMap data, header;
@@ -51,27 +45,16 @@ public class DashboardFragment extends Fragment {
     }
 
     private void initializer() {
-        cases2Adapter = new Cases2Adapter(requireActivity());
-        indexSampleAdapter = new IndexSampleAdapter(requireActivity());
         roomsAdapter = new RoomsAdapter(requireActivity());
-        centersAdapter = new CentersAdapter(requireActivity());
 
         data = new HashMap<>();
         data.put("user", ((MainActivity) requireActivity()).singleton.getId());
         header = new HashMap<>();
         header.put("Authorization", ((MainActivity) requireActivity()).singleton.getAuthorization());
 
-        binding.casesHeaderLayout.titleTextView.setText(getResources().getString(R.string.DashboardFragmentCasesHeader));
-        binding.samplesHeaderLayout.titleTextView.setText(getResources().getString(R.string.DashboardFragmentSamplesHeader));
         binding.roomsHeaderLayout.titleTextView.setText(getResources().getString(R.string.DashboardFragmentRoomsHeader));
-        binding.centersHeaderLayout.titleTextView.setText(getResources().getString(R.string.DashboardFragmentCentersHeader));
 
-        binding.samplesShimmerLayout.shimmerItem1.borderView.setVisibility(View.GONE);
-
-        InitManager.fixedVerticalRecyclerView(requireActivity(), binding.casesSingleLayout.recyclerView, getResources().getDimension(R.dimen._12sdp), 0, getResources().getDimension(R.dimen._4sdp), getResources().getDimension(R.dimen._12sdp));
-        InitManager.fixedVerticalRecyclerView(requireActivity(), binding.samplesSingleLayout.recyclerView, 0, 0, 0, 0);
         InitManager.fixedVerticalRecyclerView(requireActivity(), binding.roomsSingleLayout.recyclerView, getResources().getDimension(R.dimen._12sdp), 0, getResources().getDimension(R.dimen._4sdp), getResources().getDimension(R.dimen._12sdp));
-        InitManager.fixedVerticalRecyclerView(requireActivity(), binding.centersSingleLayout.recyclerView, getResources().getDimension(R.dimen._12sdp), 0, getResources().getDimension(R.dimen._4sdp), getResources().getDimension(R.dimen._12sdp));
     }
 
     private void getData() {
@@ -83,34 +66,6 @@ public class DashboardFragment extends Fragment {
                 if (isAdded()) {
                     requireActivity().runOnUiThread(() -> {
                         ((MainActivity) requireActivity()).singleton.update(model);
-
-                        // Cases Data
-                        if (!model.getCaseList().data().isEmpty()) {
-                            cases2Adapter.setItems(model.getCaseList().data());
-
-                            binding.casesSingleLayout.recyclerView.setAdapter(cases2Adapter);
-                            binding.casesHeaderLayout.countTextView.setText(StringManager.bracing(cases2Adapter.getItemCount()));
-
-                            binding.casesSingleLayout.getRoot().setVisibility(View.VISIBLE);
-                        } else if (cases2Adapter.getItemCount() == 0) {
-                            binding.casesGroup.setVisibility(View.GONE);
-                        }
-                        binding.casesShimmerLayout.getRoot().setVisibility(View.GONE);
-                        binding.casesShimmerLayout.getRoot().stopShimmer();
-
-                        // Samples Data
-                        if (!model.getSampleList().data().isEmpty()) {
-                            indexSampleAdapter.setItems(model.getSampleList().data());
-
-                            binding.samplesSingleLayout.recyclerView.setAdapter(indexSampleAdapter);
-                            binding.samplesHeaderLayout.countTextView.setText(StringManager.bracing(indexSampleAdapter.itemsCount()));
-
-                            binding.samplesSingleLayout.getRoot().setVisibility(View.VISIBLE);
-                        } else if (indexSampleAdapter.getItemCount() == 0) {
-                            binding.samplesGroup.setVisibility(View.GONE);
-                        }
-                        binding.samplesShimmerLayout.getRoot().setVisibility(View.GONE);
-                        binding.samplesShimmerLayout.getRoot().stopShimmer();
 
                         // Rooms Data
                         if (!model.getRoomList().data().isEmpty()) {
@@ -126,20 +81,6 @@ public class DashboardFragment extends Fragment {
                         binding.roomsShimmerLayout.getRoot().setVisibility(View.GONE);
                         binding.roomsShimmerLayout.getRoot().stopShimmer();
 
-                        // Centers Data
-                        if (!model.getCenterList().data().isEmpty()) {
-                            centersAdapter.setItems(model.getCenterList().data());
-
-                            binding.centersSingleLayout.recyclerView.setAdapter(centersAdapter);
-                            binding.centersHeaderLayout.countTextView.setText(StringManager.bracing(centersAdapter.getItemCount()));
-
-                            binding.centersSingleLayout.getRoot().setVisibility(View.VISIBLE);
-                        } else if (centersAdapter.getItemCount() == 0) {
-                            binding.centersGroup.setVisibility(View.GONE);
-                        }
-                        binding.centersShimmerLayout.getRoot().setVisibility(View.GONE);
-                        binding.centersShimmerLayout.getRoot().stopShimmer();
-
                     });
                 }
             }
@@ -149,25 +90,10 @@ public class DashboardFragment extends Fragment {
                 if (isAdded()) {
                     requireActivity().runOnUiThread(() -> {
 
-                        // Cases Data
-                        binding.casesGroup.setVisibility(View.GONE);
-                        binding.casesShimmerLayout.getRoot().setVisibility(View.GONE);
-                        binding.casesShimmerLayout.getRoot().stopShimmer();
-
-                        // Samples Data
-                        binding.samplesGroup.setVisibility(View.GONE);
-                        binding.samplesShimmerLayout.getRoot().setVisibility(View.GONE);
-                        binding.samplesShimmerLayout.getRoot().stopShimmer();
-
                         // Rooms Data
                         binding.roomsGroup.setVisibility(View.GONE);
                         binding.roomsShimmerLayout.getRoot().setVisibility(View.GONE);
                         binding.roomsShimmerLayout.getRoot().stopShimmer();
-
-                        // Centers Data
-                        binding.centersGroup.setVisibility(View.GONE);
-                        binding.centersShimmerLayout.getRoot().setVisibility(View.GONE);
-                        binding.centersShimmerLayout.getRoot().stopShimmer();
 
                     });
                 }
