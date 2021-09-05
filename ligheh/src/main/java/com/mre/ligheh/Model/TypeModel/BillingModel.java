871764c9c82126d721002392a69826f768f1721f
S.Mohammad Reza <36046159+hmila2000@users.noreply.github.com>
2021-09-05 08:01:50 +0430

    add billing and treasuries

---
+++ ligheh/src/main/java/com/mre/ligheh/Model/TypeModel/BillingModel.java
@@ -0,0 +1,103 @@
+package com.mre.ligheh.Model.TypeModel;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class BillingModel extends TypeModel{
+    private String id;
+    private TreasuriesModel creditor;
+    private TreasuriesModel debtor;
+    private int amount;
+    private String type;
+    private String action;
+    private String title;
+    private int created_at;
+
+    public BillingModel(JSONObject jsonObject) {
+        super(jsonObject);
+            try {
+        if (!jsonObject.isNull("id"))
+                setId(jsonObject.getString("id"));
+                if (!jsonObject.isNull("creditor"))
+                    setCreditor(new TreasuriesModel(jsonObject.getJSONObject("creditor")));
+                if (!jsonObject.isNull("debtor"))
+                    setDebtor(new TreasuriesModel(jsonObject.getJSONObject("debtor")));
+                if (!jsonObject.isNull("amount"))
+                    setAmount(jsonObject.getInt("amount"));
+                if (!jsonObject.isNull("type"))
+                    setType(jsonObject.getString("type"));
+                if (!jsonObject.isNull("action"))
+                    setAction(jsonObject.getString("action"));
+                if (!jsonObject.isNull("title"))
+                    setTitle(jsonObject.getString("title"));
+                if (!jsonObject.isNull("created_at"))
+                    setCreated_at(jsonObject.getInt("created_at"));
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public TreasuriesModel getCreditor() {
+        return creditor;
+    }
+
+    public void setCreditor(TreasuriesModel creditor) {
+        this.creditor = creditor;
+    }
+
+    public TreasuriesModel getDebtor() {
+        return debtor;
+    }
+
+    public void setDebtor(TreasuriesModel debtor) {
+        this.debtor = debtor;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getAction() {
+        return action;
+    }
+
+    public void setAction(String action) {
+        this.action = action;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(int created_at) {
+        this.created_at = created_at;
+    }
+}
---
+++ ligheh/src/main/java/com/mre/ligheh/Model/TypeModel/TreasuriesModel.java
@@ -0,0 +1,103 @@
+package com.mre.ligheh.Model.TypeModel;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class TreasuriesModel extends TypeModel {
+    private String id;
+    private String symbol;
+    private String title;
+    private boolean creditable;
+    private boolean my_treasury;
+    private int balance;
+    private UserModel userModel;
+    private CenterModel centerModel;
+
+    public TreasuriesModel(JSONObject jsonObject) {
+        super(jsonObject);
+        try {
+            if (!jsonObject.isNull("id"))
+                setId(jsonObject.getString("id"));
+            if (!jsonObject.isNull("symbol"))
+                setSymbol(jsonObject.getString("symbol"));
+            if (!jsonObject.isNull("title"))
+                setTitle(jsonObject.getString("title"));
+            if (!jsonObject.isNull("creditable"))
+                setCreditable(jsonObject.getBoolean("creditable"));
+            if (!jsonObject.isNull("my_treasury"))
+                setMy_treasury(jsonObject.getBoolean("my_treasury"));
+            if (!jsonObject.isNull("balance"))
+                setBalance(jsonObject.getInt("balance"));
+            if (!jsonObject.isNull("center"))
+                setCenterModel(new CenterModel(jsonObject.getJSONObject("center")));
+            if (!jsonObject.isNull("user"))
+                setUserModel(new UserModel(jsonObject.getJSONObject("user")));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public void setSymbol(String symbol) {
+        this.symbol = symbol;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public boolean isCreditable() {
+        return creditable;
+    }
+
+    public void setCreditable(boolean creditable) {
+        this.creditable = creditable;
+    }
+
+    public boolean isMy_treasury() {
+        return my_treasury;
+    }
+
+    public void setMy_treasury(boolean my_treasury) {
+        this.my_treasury = my_treasury;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+
+    public UserModel getUserModel() {
+        return userModel;
+    }
+
+    public void setUserModel(UserModel userModel) {
+        this.userModel = userModel;
+    }
+
+    public CenterModel getCenterModel() {
+        return centerModel;
+    }
+
+    public void setCenterModel(CenterModel centerModel) {
+        this.centerModel = centerModel;
+    }
+}
