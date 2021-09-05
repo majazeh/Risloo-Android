91f6c7b65f4220d40168057cdea343ada9a07f26
Amir Dehbashi <xamirdehbashix@gmail.com>
2021-09-05 18:01:08 +0430

    redesigned dashboard
    part 2

--- app/src/main/java/com/majazeh/risloo/Views/Fragments/Show/DashboardFragment.java
+++ app/src/main/java/com/majazeh/risloo/Views/Fragments/Show/DashboardFragment.java
@@ -1,5 +1,6 @@
 package com.majazeh.risloo.Views.Fragments.Show;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,10 +9,13 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavDirections;
 
+import com.majazeh.risloo.NavigationMainDirections;
 import com.majazeh.risloo.R;
 import com.majazeh.risloo.Utils.Managers.InitManager;
 import com.majazeh.risloo.Utils.Managers.StringManager;
+import com.majazeh.risloo.Utils.Widgets.CustomClickView;
 import com.majazeh.risloo.Views.Activities.MainActivity;
 import com.majazeh.risloo.Views.Adapters.Recycler.RoomsAdapter;
 import com.majazeh.risloo.databinding.FragmentDashboardBinding;
@@ -29,6 +33,9 @@ public class DashboardFragment extends Fragment {
     // Adapters
     private RoomsAdapter roomsAdapter;
 
+    // Models
+    private UserModel userModel;
+
     // Objects
     private HashMap data, header;
 
@@ -38,6 +45,10 @@ public class DashboardFragment extends Fragment {
         binding = FragmentDashboardBinding.inflate(inflater, viewGroup, false);
 
         initializer();
+
+        detector();
+
+        listener();
 
         getData();
 
@@ -52,35 +63,73 @@ public class DashboardFragment extends Fragment {
         header = new HashMap<>();
         header.put("Authorization", ((MainActivity) requireActivity()).singleton.getAuthorization());
 
+        binding.passwordMissingLayout.titleTextView.setText(getResources().getString(R.string.DashboardFragmentNoPasswordTitle));
+        binding.centerMissingLayout.titleTextView.setText(getResources().getString(R.string.DashboardFragmentNoCenterTitle));
+
+        binding.passwordMissingLayout.actionTextView.setText(getResources().getString(R.string.DashboardFragmentNoPasswordAction));
+        binding.centerMissingLayout.actionTextView.setText(getResources().getString(R.string.DashboardFragmentNoCenterAction));
+
         binding.roomsHeaderLayout.titleTextView.setText(getResources().getString(R.string.DashboardFragmentRoomsHeader));
 
         InitManager.fixedVerticalRecyclerView(requireActivity(), binding.roomsSingleLayout.recyclerView, getResources().getDimension(R.dimen._12sdp), 0, getResources().getDimension(R.dimen._4sdp), getResources().getDimension(R.dimen._12sdp));
+    }
+
+    private void detector() {
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP) {
+            binding.passwordMissingLayout.actionTextView.setBackgroundResource(R.drawable.draw_16sdp_solid_white_border_1sdp_blue600_ripple_blue300);
+            binding.centerMissingLayout.actionTextView.setBackgroundResource(R.drawable.draw_16sdp_solid_white_border_1sdp_blue600_ripple_blue300);
+        } else {
+            binding.passwordMissingLayout.actionTextView.setBackgroundResource(R.drawable.draw_16sdp_solid_transparent_border_1sdp_blue600);
+            binding.centerMissingLayout.actionTextView.setBackgroundResource(R.drawable.draw_16sdp_solid_transparent_border_1sdp_blue600);
+        }
+    }
+
+    private void listener() {
+        CustomClickView.onClickListener(() -> {
+            NavDirections action = NavigationMainDirections.actionGlobalEditUserFragment(userModel);
+            ((MainActivity) requireActivity()).navController.navigate(action);
+        }).widget(binding.passwordMissingLayout.actionTextView);
+
+        CustomClickView.onClickListener(() -> {
+            NavDirections action = NavigationMainDirections.actionGlobalCentersFragment();
+            ((MainActivity) requireActivity()).navController.navigate(action);
+        }).widget(binding.centerMissingLayout.actionTextView);
+    }
+
+    private void setData(UserModel model) {
+        ((MainActivity) requireActivity()).singleton.update(model);
+
+        if (model.isNo_password())
+            binding.passwordMissingLayout.getRoot().setVisibility(View.VISIBLE);
+        else
+            binding.passwordMissingLayout.getRoot().setVisibility(View.GONE);
+
+        if (binding.passwordMissingLayout.getRoot().getVisibility() == View.VISIBLE || binding.centerMissingLayout.getRoot().getVisibility() == View.VISIBLE)
+            binding.missingSpace.setVisibility(View.VISIBLE);
+        else
+            binding.missingSpace.setVisibility(View.GONE);
+
+        if (!model.getRoomList().data().isEmpty()) {
+            roomsAdapter.setItems(model.getRoomList().data());
+
+            binding.roomsSingleLayout.recyclerView.setAdapter(roomsAdapter);
+            binding.roomsHeaderLayout.countTextView.setText(StringManager.bracing(roomsAdapter.getItemCount()));
+
+            binding.roomsGroup.setVisibility(View.VISIBLE);
+        } else if (roomsAdapter.getItemCount() == 0) {
+            binding.roomsGroup.setVisibility(View.GONE);
+        }
     }
 
     private void getData() {
         User.dashboard(data, header, new Response() {
             @Override
             public void onOK(Object object) {
-                UserModel model = (UserModel) object;
+                userModel = (UserModel) object;
 
                 if (isAdded()) {
                     requireActivity().runOnUiThread(() -> {
-                        ((MainActivity) requireActivity()).singleton.update(model);
-
-                        // Rooms Data
-                        if (!model.getRoomList().data().isEmpty()) {
-                            roomsAdapter.setItems(model.getRoomList().data());
-
-                            binding.roomsSingleLayout.recyclerView.setAdapter(roomsAdapter);
-                            binding.roomsHeaderLayout.countTextView.setText(StringManager.bracing(roomsAdapter.getItemCount()));
-
-                            binding.roomsSingleLayout.getRoot().setVisibility(View.VISIBLE);
-                        } else if (roomsAdapter.getItemCount() == 0) {
-                            binding.roomsGroup.setVisibility(View.GONE);
-                        }
-                        binding.roomsShimmerLayout.getRoot().setVisibility(View.GONE);
-                        binding.roomsShimmerLayout.getRoot().stopShimmer();
-
+                        setData(userModel);
                     });
                 }
             }
@@ -89,12 +138,7 @@ public class DashboardFragment extends Fragment {
             public void onFailure(String response) {
                 if (isAdded()) {
                     requireActivity().runOnUiThread(() -> {
-
-                        // Rooms Data
-                        binding.roomsGroup.setVisibility(View.GONE);
-                        binding.roomsShimmerLayout.getRoot().setVisibility(View.GONE);
-                        binding.roomsShimmerLayout.getRoot().stopShimmer();
-
+                        // TODO : Place Code If Needed
                     });
                 }
             }
