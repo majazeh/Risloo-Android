f8a2c1a99ecd6ccc662bbd09485f99bd9cbd634b
Amir Dehbashi <xamirdehbashix@gmail.com>
2021-09-09 17:43:02 +0430

    create treasury nearly finished

--- app/src/main/java/com/majazeh/risloo/Views/Fragments/Create/CreateTreasuryFragment.java
+++ app/src/main/java/com/majazeh/risloo/Views/Fragments/Create/CreateTreasuryFragment.java
@@ -14,13 +14,21 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.majazeh.risloo.R;
+import com.majazeh.risloo.Utils.Managers.DialogManager;
+import com.majazeh.risloo.Utils.Managers.SnackManager;
 import com.majazeh.risloo.Utils.Widgets.CustomClickView;
 import com.majazeh.risloo.Utils.Managers.InitManager;
 import com.majazeh.risloo.Views.Activities.MainActivity;
 import com.majazeh.risloo.databinding.FragmentCreateTreasuryBinding;
+import com.mre.ligheh.API.Response;
+import com.mre.ligheh.Model.Madule.Treasury;
 import com.mre.ligheh.Model.TypeModel.TypeModel;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.HashMap;
+import java.util.Iterator;
 
 public class CreateTreasuryFragment extends Fragment {
 
@@ -133,64 +141,64 @@ public class CreateTreasuryFragment extends Fragment {
     }
 
     private void doWork() {
-//        DialogManager.showLoadingDialog(requireActivity(), "loading");
-//
-//        data.put("title", title);
-//        data.put("region_id", regionId);
-//
-//        Treasury.create(data, header, new Response() {
-//            @Override
-//            public void onOK(Object object) {
-//                if (isAdded()) {
-//                    requireActivity().runOnUiThread(() -> {
-//                        DialogManager.dismissLoadingDialog();
-//                        SnackManager.showSuccesSnack(requireActivity(), getResources().getString(R.string.ToastNewTreasuryAdded));
-//
-//                        ((MainActivity) requireActivity()).navController.navigateUp();
-//                    });
-//                }
-//            }
-//
-//            @Override
-//            public void onFailure(String response) {
-//                if (isAdded()) {
-//                    requireActivity().runOnUiThread(() -> {
-//                        try {
-//                            JSONObject responseObject = new JSONObject(response);
-//                            if (!responseObject.isNull("errors")) {
-//                                JSONObject errorsObject = responseObject.getJSONObject("errors");
-//
-//                                Iterator<String> keys = (errorsObject.keys());
-//                                StringBuilder errors = new StringBuilder();
-//
-//                                while (keys.hasNext()) {
-//                                    String key = keys.next();
-//                                    for (int i = 0; i < errorsObject.getJSONArray(key).length(); i++) {
-//                                        String validation = errorsObject.getJSONArray(key).get(i).toString();
-//
-//                                        switch (key) {
-//                                            case "title":
-//                                                ((MainActivity) requireActivity()).validatoon.showValid(binding.titleErrorLayout.getRoot(), binding.titleErrorLayout.errorTextView, validation);
-//                                                break;
-//                                            case "region_id":
-//                                                ((MainActivity) requireActivity()).validatoon.showValid(binding.regionErrorLayout.getRoot(), binding.regionErrorLayout.errorTextView, validation);
-//                                                break;
-//                                        }
-//
-//                                        errors.append(validation);
-//                                        errors.append("\n");
-//                                    }
-//                                }
-//
-//                                SnackManager.showErrorSnack(requireActivity(), errors.substring(0, errors.length() - 1));
-//                            }
-//                        } catch (JSONException e) {
-//                            e.printStackTrace();
-//                        }
-//                    });
-//                }
-//            }
-//        });
+        DialogManager.showLoadingDialog(requireActivity(), "loading");
+
+        data.put("title", title);
+        data.put("region_id", regionId);
+
+        Treasury.create(data, header, new Response() {
+            @Override
+            public void onOK(Object object) {
+                if (isAdded()) {
+                    requireActivity().runOnUiThread(() -> {
+                        DialogManager.dismissLoadingDialog();
+                        SnackManager.showSuccesSnack(requireActivity(), getResources().getString(R.string.ToastNewTreasuryAdded));
+
+                        ((MainActivity) requireActivity()).navController.navigateUp();
+                    });
+                }
+            }
+
+            @Override
+            public void onFailure(String response) {
+                if (isAdded()) {
+                    requireActivity().runOnUiThread(() -> {
+                        try {
+                            JSONObject responseObject = new JSONObject(response);
+                            if (!responseObject.isNull("errors")) {
+                                JSONObject errorsObject = responseObject.getJSONObject("errors");
+
+                                Iterator<String> keys = (errorsObject.keys());
+                                StringBuilder errors = new StringBuilder();
+
+                                while (keys.hasNext()) {
+                                    String key = keys.next();
+                                    for (int i = 0; i < errorsObject.getJSONArray(key).length(); i++) {
+                                        String validation = errorsObject.getJSONArray(key).get(i).toString();
+
+                                        switch (key) {
+                                            case "title":
+                                                ((MainActivity) requireActivity()).validatoon.showValid(binding.titleErrorLayout.getRoot(), binding.titleErrorLayout.errorTextView, validation);
+                                                break;
+                                            case "region_id":
+                                                ((MainActivity) requireActivity()).validatoon.showValid(binding.regionErrorLayout.getRoot(), binding.regionErrorLayout.errorTextView, validation);
+                                                break;
+                                        }
+
+                                        errors.append(validation);
+                                        errors.append("\n");
+                                    }
+                                }
+
+                                SnackManager.showErrorSnack(requireActivity(), errors.substring(0, errors.length() - 1));
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    });
+                }
+            }
+        });
     }
 
     @Override
