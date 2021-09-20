87c4d7cf2186ae4387a0533b57b98a99f0bc6013
S.Mohammad Reza <36046159+hmila2000@users.noreply.github.com>
2021-09-20 18:09:41 +0430

    Update Billing.java

--- ligheh/src/main/java/com/mre/ligheh/Model/Madule/Billing.java
+++ ligheh/src/main/java/com/mre/ligheh/Model/Madule/Billing.java
@@ -2,6 +2,7 @@ package com.mre.ligheh.Model.Madule;
 
 import com.mre.ligheh.API.Response;
 import com.mre.ligheh.Model.TypeModel.BillingModel;
+import com.mre.ligheh.Model.TypeModel.PaymentModel;
 
 import org.json.JSONObject;
 
@@ -21,6 +22,14 @@ public class Billing extends Model {
     public static void list(HashMap<String, Object> data, HashMap<String, Object> header, Response response)  {
         try {
             Model.list(endpoint, data, header, response, BillingModel.class);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void settled(HashMap<String, Object> data, HashMap<String, Object> header, Response response) {
+        try {
+            Model.post(endpoint + "/" + data.get("id") + "/settled", data, header, response, PaymentModel.class);
         } catch (IOException e) {
             e.printStackTrace();
         }
