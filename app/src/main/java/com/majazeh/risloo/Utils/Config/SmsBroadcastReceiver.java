0bd96f633e950f3e5a7b52de849be718423b2632
Amir Dehbashi <a.dehbashi100@gmail.com>
2021-11-10 12:30:00 +0330

    fixed some stuffs

--- app/src/main/java/com/majazeh/risloo/Utils/Config/SmsBroadcastReceiver.java
+++ app/src/main/java/com/majazeh/risloo/Utils/Config/SmsBroadcastReceiver.java
@@ -1,19 +1,13 @@
 package com.majazeh.risloo.Utils.Config;
 
-import android.app.Activity;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 
-import androidx.fragment.app.Fragment;
-
 import com.google.android.gms.auth.api.phone.SmsRetriever;
 import com.google.android.gms.common.api.CommonStatusCodes;
 import com.google.android.gms.common.api.Status;
-import com.majazeh.risloo.Utils.Managers.SnackManager;
-import com.majazeh.risloo.Views.Activities.AuthActivity;
-import com.majazeh.risloo.Views.Fragments.Auth.AuthPinFragment;
 
 public class SmsBroadcastReceiver extends BroadcastReceiver {
 
@@ -25,19 +19,13 @@ public class SmsBroadcastReceiver extends BroadcastReceiver {
 
             switch(status.getStatusCode()) {
                 case CommonStatusCodes.SUCCESS:
+                    System.out.println("onReceiveSucces: " + status.getStatusMessage());
                     String message = (String) extras.get(SmsRetriever.EXTRA_SMS_MESSAGE);
 
-                    if (context instanceof AuthActivity) {
-                        Fragment current = ((AuthActivity) context).fragmont.getCurrent();
-
-                        if (current instanceof AuthPinFragment) {
-                            // TODO : Extract the code from the message and pass to the input here
-                        }
-                    }
+                    // TODO : Extract the code from the message and pass to the input here
                     break;
-                case CommonStatusCodes.TIMEOUT:
+                default:
                     System.out.println("onReceiveFail: " + status.getStatusMessage());
-                    SnackManager.showDefaultSnack((Activity) context, status.getStatusMessage());
                     break;
             }
         }
