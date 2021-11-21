ef237a2e77450d2ca6923c9150b6ccbb6266a509
Amir Dehbashi <a.dehbashi100@gmail.com>
2021-11-21 19:03:14 +0330

    fixed some suffs

--- app/src/main/java/com/majazeh/risloo/Utils/Config/SmsBroadcastReceiver.java
+++ app/src/main/java/com/majazeh/risloo/Utils/Config/SmsBroadcastReceiver.java
@@ -9,6 +9,9 @@ import com.google.android.gms.auth.api.phone.SmsRetriever;
 import com.google.android.gms.common.api.CommonStatusCodes;
 import com.google.android.gms.common.api.Status;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class SmsBroadcastReceiver extends BroadcastReceiver {
 
     @Override
@@ -20,11 +23,17 @@ public class SmsBroadcastReceiver extends BroadcastReceiver {
             switch(status.getStatusCode()) {
                 case CommonStatusCodes.SUCCESS:
                     System.out.println("onReceiveSucces: " + status.getStatusMessage());
-
                     String message = (String) extras.get(SmsRetriever.EXTRA_SMS_MESSAGE);
-                    String code = message.substring(5, 11);
 
-                    // TODO : Extract the code from the message and pass to the input here
+                    Pattern pattern = Pattern.compile("\\d+");
+                    Matcher matcher = pattern.matcher(message);
+
+                    if (matcher.find()) {
+                        String code = matcher.group(0);
+
+                        // TODO : Extract the code from the message and pass to the input here
+                    }
+
                     break;
                 default:
                     System.out.println("onReceiveFail: " + status.getStatusMessage());
