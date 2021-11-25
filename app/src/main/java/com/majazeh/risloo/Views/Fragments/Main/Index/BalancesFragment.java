bc7ae3dcb35e5810109dde3f11a7aa9f20cadd43
Amir Dehbashi <a.dehbashi100@gmail.com>
2021-11-25 18:01:37 +0330

    added commissions & balances index
    part 3

--- app/src/main/java/com/majazeh/risloo/Views/Fragments/Main/Index/BalancesFragment.java
+++ app/src/main/java/com/majazeh/risloo/Views/Fragments/Main/Index/BalancesFragment.java
@@ -1,5 +1,6 @@
 package com.majazeh.risloo.Views.Fragments.Main.Index;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,12 +10,27 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.majazeh.risloo.R;
+import com.majazeh.risloo.Utils.Managers.InitManager;
+import com.majazeh.risloo.Views.Activities.MainActivity;
 import com.majazeh.risloo.databinding.FragmentBalancesBinding;
+
+import java.util.HashMap;
+import java.util.Objects;
 
 public class BalancesFragment extends Fragment {
 
     // Binding
     private FragmentBalancesBinding binding;
+
+//    // Adapters
+//    private TableBalanceAdapter adapter;
+
+    // Objects
+    private HashMap data, header;
+
+    // Vars
+    private boolean isLoading = true;
 
     @Nullable
     @Override
@@ -23,23 +39,106 @@ public class BalancesFragment extends Fragment {
 
         initializer();
 
-        setData();
+        listener();
+
+        getData();
 
         return binding.getRoot();
     }
 
     private void initializer() {
-        // TODO : Place Code When Needed
+//        adapter = new TableBalanceAdapter(requireActivity());
+
+        data = new HashMap<>();
+        data.put("page", 1);
+        header = new HashMap<>();
+        header.put("Authorization", ((MainActivity) requireActivity()).singleton.getAuthorization());
+
+        binding.headerIncludeLayout.titleTextView.setText(getResources().getString(R.string.BalancesFragmentTitle));
+
+        binding.tableShimmerLayout.shimmerItem1.borderView.setVisibility(View.GONE);
+
+        InitManager.fixedVerticalRecyclerView(requireActivity(), binding.tableSingleLayout.recyclerView, 0, 0, 0, 0);
     }
 
-    private void setData() {
-        // TODO : Place Code When Needed
+    @SuppressLint("ClickableViewAccessibility")
+    private void listener() {
+        binding.getRoot().setMOnScrollChangeListener((v, scrollX, scrollY, oldScrollX, oldScrollY) -> {
+            if (!isLoading && !Objects.requireNonNull(v).canScrollVertically(1)) {
+                isLoading = true;
+
+                if (data.containsKey("page"))
+                    data.put("page", ((int) data.get("page")) + 1);
+                else
+                    data.put("page", 1);
+
+                if (binding.tableSingleLayout.progressBar.getVisibility() == View.GONE)
+                    binding.tableSingleLayout.progressBar.setVisibility(View.VISIBLE);
+
+                getData();
+            }
+        });
+    }
+
+    private void getData() {
+//        Balance.list(data, header, new Response() {
+//            @Override
+//            public void onOK(Object object) {
+//                List items = (List) object;
+//
+//                if (isAdded()) {
+//                    requireActivity().runOnUiThread(() -> {
+//                        if (Objects.equals(data.get("page"), 1))
+//                            adapter.clearItems();
+//
+//                        if (!items.data().isEmpty()) {
+//                            adapter.setItems(items.data());
+//                            binding.tableSingleLayout.recyclerView.setAdapter(adapter);
+//
+//                            binding.tableSingleLayout.emptyView.setVisibility(View.GONE);
+//                        } else if (adapter.itemsCount() == 0) {
+//                            binding.tableSingleLayout.recyclerView.setAdapter(null);
+//
+//                            binding.tableSingleLayout.emptyView.setVisibility(View.VISIBLE);
+//                            binding.tableSingleLayout.emptyView.setText(getResources().getString(R.string.BalancesFragmentEmpty));
+//                        }
+//
+//                        binding.headerIncludeLayout.countTextView.setText(StringManager.bracing(items.getTotal()));
+//
+//                        hideShimmer();
+//                    });
+//
+//                    isLoading = false;
+//                }
+//            }
+//
+//            @Override
+//            public void onFailure(String response) {
+//                if (isAdded()) {
+//                    requireActivity().runOnUiThread(() -> {
+//                        hideShimmer();
+//                    });
+//
+//                    isLoading = false;
+//                }
+//            }
+//        });
+    }
+
+    private void hideShimmer() {
+        binding.tableSingleLayout.getRoot().setVisibility(View.VISIBLE);
+        binding.tableShimmerLayout.getRoot().setVisibility(View.GONE);
+        binding.tableShimmerLayout.getRoot().stopShimmer();
+
+        if (binding.tableSingleLayout.progressBar.getVisibility() == View.VISIBLE)
+            binding.tableSingleLayout.progressBar.setVisibility(View.GONE);
     }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+        isLoading = true;
     }
 
 }
