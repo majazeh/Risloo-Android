8379d46bc62ae78b25d32ffb9f1c50d9a0e84d40
Amir Dehbashi <a.dehbashi100@gmail.com>
2022-03-09 01:59:37 +0330

    Cleaned PermissionManager

--- app/src/main/java/com/majazeh/risloo/Utils/Managers/PermissionManager.java
+++ app/src/main/java/com/majazeh/risloo/Utils/Managers/PermissionManager.java
@@ -16,12 +16,13 @@ public class PermissionManager {
 
     public static boolean file(Activity activity) {
         String[] permissions = {Manifest.permission.READ_EXTERNAL_STORAGE};
+        int requestCode = 100;
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            if (ContextCompat.checkSelfPermission(activity, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+            if (ContextCompat.checkSelfPermission(activity, permissions[0]) == PackageManager.PERMISSION_GRANTED) {
                 return true;
             } else {
-                ActivityCompat.requestPermissions(activity, permissions, 100);
+                ActivityCompat.requestPermissions(activity, permissions, requestCode);
                 return false;
             }
         } else {
@@ -31,12 +32,13 @@ public class PermissionManager {
 
     public static boolean storage(Activity activity) {
         String[] permissions = {Manifest.permission.WRITE_EXTERNAL_STORAGE};
+        int requestCode = 200;
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            if (ContextCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+            if (ContextCompat.checkSelfPermission(activity, permissions[0]) == PackageManager.PERMISSION_GRANTED) {
                 return true;
             } else {
-                ActivityCompat.requestPermissions(activity, permissions, 200);
+                ActivityCompat.requestPermissions(activity, permissions, requestCode);
                 return false;
             }
         } else {
@@ -46,12 +48,13 @@ public class PermissionManager {
 
     public static boolean gallery(Activity activity) {
         String[] permissions = {Manifest.permission.READ_EXTERNAL_STORAGE};
+        int requestCode = 300;
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            if (ContextCompat.checkSelfPermission(activity, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+            if (ContextCompat.checkSelfPermission(activity, permissions[0]) == PackageManager.PERMISSION_GRANTED) {
                 return true;
             } else {
-                ActivityCompat.requestPermissions(activity, permissions, 300);
+                ActivityCompat.requestPermissions(activity, permissions, requestCode);
                 return false;
             }
         } else {
@@ -61,24 +64,25 @@ public class PermissionManager {
 
     public static boolean camera(Activity activity) {
         String[] permissions = {Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE};
+        int requestCode = 400;
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            if (ContextCompat.checkSelfPermission(activity, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
-                if (ContextCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+            if (ContextCompat.checkSelfPermission(activity, permissions[0]) == PackageManager.PERMISSION_GRANTED) {
+                if (ContextCompat.checkSelfPermission(activity, permissions[1]) == PackageManager.PERMISSION_GRANTED) {
                     return true;
                 } else {
-                    ActivityCompat.requestPermissions(activity, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 400);
+                    ActivityCompat.requestPermissions(activity, new String[]{permissions[1]}, requestCode);
                     return false;
                 }
-            } else if (ContextCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
-                if (ContextCompat.checkSelfPermission(activity, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+            } else if (ContextCompat.checkSelfPermission(activity, permissions[1]) == PackageManager.PERMISSION_GRANTED) {
+                if (ContextCompat.checkSelfPermission(activity, permissions[0]) == PackageManager.PERMISSION_GRANTED) {
                     return true;
                 } else {
-                    ActivityCompat.requestPermissions(activity, new String[]{Manifest.permission.CAMERA}, 400);
+                    ActivityCompat.requestPermissions(activity, new String[]{permissions[0]}, requestCode);
                     return false;
                 }
             } else {
-                ActivityCompat.requestPermissions(activity, permissions, 400);
+                ActivityCompat.requestPermissions(activity, permissions, requestCode);
                 return false;
             }
         } else {
