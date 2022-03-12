a91ceeebff7bf38619163090d75654fd5fd9cb49
Amir Dehbashi <a.dehbashi100@gmail.com>
2022-03-12 19:57:48 +0330

    fixing classes
    part 21

--- app/src/main/java/com/majazeh/risloo/Utils/Managers/PathManager.java
+++ app/src/main/java/com/majazeh/risloo/Utils/Managers/PathManager.java
@@ -22,118 +22,185 @@ import java.util.Objects;
 public class PathManager {
 
     /*
-    ---------- Funcs ----------
+    ---------- Func's ----------
     */
 
     public static String localPath(Activity activity, Uri uri) {
+        if (isDocumentUri(activity, uri)) {
 
-        // DocumentProvider
-        if (DocumentsContract.isDocumentUri(activity, uri)) {
+            if (isLocalStorageDocumentUri(uri))
+                return getLocalStorageDocument(uri);
 
-            // Local
-            if (isLocalStorageDocument(uri)) {
-                return DocumentsContract.getDocumentId(uri);
-            }
+            else if (isExternalStorageDocumentUri(uri))
+                return getExternalStorageDocument(uri);
 
-            // External
-            else if (isExternalStorageDocument(uri)) {
-                String documentId = DocumentsContract.getDocumentId(uri);
-                String[] split = documentId.split(":");
-                String type = split[0];
+            else if (isDownloadsDocumentUri(uri))
+                return getDownloadsDocument(activity, uri);
 
-                if ("primary".equalsIgnoreCase(type))
-                    return Environment.getExternalStorageDirectory() + "/" + split[1];
-                else if ("home".equalsIgnoreCase(type))
-                    return Environment.getExternalStorageDirectory() + "/documents/" + split[1];
-            }
-
-            // Downloads
-            else if (isDownloadsDocument(uri)) {
-                String documentId = DocumentsContract.getDocumentId(uri);
-
-                if (documentId != null && documentId.startsWith("raw:"))
-                    return documentId.substring(4);
-
-                String[] contentUriPrefixesToTry = new String[] {
-                        "content://downloads/public_downloads",
-                        "content://downloads/my_downloads"
-                };
-
-                for (String contentUriPrefix : contentUriPrefixesToTry) {
-                    String[] split = Objects.requireNonNull(documentId).split(":");
-
-                    Uri contentUri;
-                    if (Objects.requireNonNull(documentId).startsWith("msf:"))
-                        contentUri = ContentUris.withAppendedId(Uri.parse(contentUriPrefix), Long.parseLong(split[1]));
-                    else
-                        contentUri = ContentUris.withAppendedId(Uri.parse(contentUriPrefix), Long.parseLong(documentId));
-
-                    String path = fileColumn(activity, contentUri, null, null);
-                    if (path != null)
-                        return path;
-                }
-
-                String fileName = fileName(activity, uri);
-                if (fileName != null) {
-                    File file = new File(FileManager.createInternalCachePath(activity, "documents"), fileName);
-                    String path = file.getAbsolutePath();
-                    writeUriToInternalCache(activity, uri, path);
-                    return path;
-                }
-            }
-
-            // Media
-            else if (isMediaDocument(uri)) {
-                String documentId = DocumentsContract.getDocumentId(uri);
-                String[] split = documentId.split(":");
-                String type = split[0];
-
-                Uri contentUri;
-                switch (type) {
-                    case "image":
-                        contentUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
-                        break;
-                    case "video":
-                        contentUri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
-                        break;
-                    case "audio":
-                        contentUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
-                        break;
-                    default:
-                        contentUri = null;
-                        break;
-                }
-
-                String selection = "_id=?";
-                String[] selectionArgs = new String[] {split[1]};
-
-                return fileColumn(activity, contentUri, selection, selectionArgs);
-            }
+            else if (isMediaDocumentUri(uri))
+                return getMediaDocument(activity, uri);
 
         }
 
-        // MediaStore
-        else if ("content".equalsIgnoreCase(uri.getScheme())) {
-            if (isGooglePhotosUri(uri))
-                return uri.getLastPathSegment();
-            else
-                return fileColumn(activity, uri, null, null);
-        }
+        else if (isMediaStoreUri(uri))
+            return getMediaStore(activity, uri);
 
-        // File
-        else if ("file".equalsIgnoreCase(uri.getScheme())) {
-            return uri.getPath();
-        }
+        else if (isFileUri(uri))
+            return getFile(uri);
 
         return null;
     }
 
     /*
-    ---------- Access ----------
+    ---------- Getter's ----------
+    */
+
+    // -------------------- Document
+
+    private static String getLocalStorageDocument(Uri uri) {
+        return DocumentsContract.getDocumentId(uri);
+    }
+
+    private static String getExternalStorageDocument(Uri uri) {
+        String docId = DocumentsContract.getDocumentId(uri);
+        String[] split = docId.split(":");
+        String type = split[0];
+
+        if (type.equalsIgnoreCase("primary"))
+            return Environment.getExternalStorageDirectory() + "/" + split[1];
+        else if (type.equalsIgnoreCase("home"))
+            return Environment.getExternalStorageDirectory() + "/documents/" + split[1];
+        else
+            return "";
+    }
+
+    private static String getDownloadsDocument(Activity activity, Uri uri) {
+        String docId = DocumentsContract.getDocumentId(uri);
+
+        if (docId != null && docId.startsWith("raw:"))
+            return docId.substring(4);
+
+        String[] contentUriPrefixesToTry = new String[]{
+                "content://downloads/public_downloads",
+                "content://downloads/my_downloads"
+        };
+
+        for (String contentUriPrefix : contentUriPrefixesToTry) {
+            String[] split = Objects.requireNonNull(docId).split(":");
+
+            Uri contentUri;
+
+            if (Objects.requireNonNull(docId).startsWith("msf:"))
+                contentUri = ContentUris.withAppendedId(Uri.parse(contentUriPrefix), Long.parseLong(split[1]));
+            else
+                contentUri = ContentUris.withAppendedId(Uri.parse(contentUriPrefix), Long.parseLong(docId));
+
+            return getDateColumn(activity, contentUri, null, null);
+        }
+
+        String fileName = getFileName(activity, uri);
+        File cacheDir = FileManager.createInternalCachePath(activity, "documents");
+        File file = new File(cacheDir, fileName);
+        String path = "";
+
+        if (fileName != null) {
+            path = file.getAbsolutePath();
+            saveUriToInternalCache(activity, uri, path);
+        }
+
+        return path;
+    }
+
+    private static String getMediaDocument(Activity activity, Uri uri) {
+        String docId = DocumentsContract.getDocumentId(uri);
+        String[] split = docId.split(":");
+        String type = split[0];
+
+        Uri contentUri = null;
+
+        switch (type) {
+            case "image":
+                contentUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+                break;
+            case "video":
+                contentUri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
+                break;
+            case "audio":
+                contentUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+                break;
+        }
+
+        String selection = "_id=?";
+        String[] selectionArgs = new String[]{
+                split[1]
+        };
+
+        return getDateColumn(activity, contentUri, selection, selectionArgs);
+    }
+
+    // -------------------- MediaStore
+
+    private static String getMediaStore(Activity activity, Uri uri) {
+        if (isGooglePhotosUri(uri))
+            return uri.getLastPathSegment();
+        else
+            return getDateColumn(activity, uri, null, null);
+    }
+
+    // -------------------- File
+
+    private static String getFile(Uri uri) {
+        return uri.getPath();
+    }
+
+    /*
+    ---------- Bool's ----------
+    */
+
+    // -------------------- Document
+
+    private static boolean isDocumentUri(Activity activity, Uri uri) {
+        return DocumentsContract.isDocumentUri(activity, uri);
+    }
+
+    private static boolean isLocalStorageDocumentUri(Uri uri) {
+        return (BuildConfig.APPLICATION_ID + ".provider").equals(uri.getAuthority());
+    }
+
+    private static boolean isExternalStorageDocumentUri(Uri uri) {
+        return "com.android.externalstorage.documents".equals(uri.getAuthority());
+    }
+
+    private static boolean isDownloadsDocumentUri(Uri uri) {
+        return "com.android.providers.downloads.documents".equals(uri.getAuthority());
+    }
+
+    private static boolean isMediaDocumentUri(Uri uri) {
+        return "com.android.providers.media.documents".equals(uri.getAuthority());
+    }
+
+    // -------------------- MediaStore
+
+    private static boolean isMediaStoreUri(Uri uri) {
+        return "content".equalsIgnoreCase(uri.getScheme());
+    }
+
+    private static boolean isGooglePhotosUri(Uri uri) {
+        return "com.google.android.apps.photos.content".equals(uri.getAuthority());
+    }
+
+    // -------------------- File
+
+    private static boolean isFileUri(Uri uri) {
+        return "file".equalsIgnoreCase(uri.getScheme());
+    }
+
+    /*
+    ---------- Save's ----------
     */
 
     @SuppressWarnings("ResultOfMethodCallIgnored")
-    private static void writeUriToInternalCache(Context context, Uri uri, String name) {
+    private static void saveUriToInternalCache(Context context, Uri uri, String name) {
         InputStream is = null;
         FileOutputStream fos = null;
         BufferedOutputStream bos = null;
@@ -153,16 +220,23 @@ public class PathManager {
             e.printStackTrace();
         } finally {
             try {
-                if (is != null) is.close();
-                if (fos != null) fos.close();
-                if (bos != null) bos.close();
+                if (is != null)
+                    is.close();
+                if (fos != null)
+                    fos.close();
+                if (bos != null)
+                    bos.close();
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
     }
 
-    private static String fileName(Context context, Uri uri) {
+    /*
+    ---------- Private's ----------
+    */
+
+    private static String getFileName(Context context, Uri uri) {
         Cursor cursor = null;
 
         try {
@@ -181,7 +255,7 @@ public class PathManager {
         return null;
     }
 
-    private static String fileColumn(Context context, Uri uri, String selection, String[] selectionArgs) {
+    private static String getDateColumn(Context context, Uri uri, String selection, String[] selectionArgs) {
         Cursor cursor = null;
 
         String column = MediaStore.Files.FileColumns.DATA;
@@ -201,30 +275,6 @@ public class PathManager {
         }
 
         return null;
-    }
-
-    /*
-    ---------- Bools ----------
-    */
-
-    private static boolean isLocalStorageDocument(Uri uri) {
-        return (BuildConfig.APPLICATION_ID + ".provider").equals(uri.getAuthority());
-    }
-
-    private static boolean isExternalStorageDocument(Uri uri) {
-        return "com.android.externalstorage.documents".equals(uri.getAuthority());
-    }
-
-    private static boolean isDownloadsDocument(Uri uri) {
-        return "com.android.providers.downloads.documents".equals(uri.getAuthority());
-    }
-
-    private static boolean isMediaDocument(Uri uri) {
-        return "com.android.providers.media.documents".equals(uri.getAuthority());
-    }
-
-    private static boolean isGooglePhotosUri(Uri uri) {
-        return "com.google.android.apps.photos.content".equals(uri.getAuthority());
     }
 
 }
