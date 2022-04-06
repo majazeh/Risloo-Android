770e296b85e1e4b53a61ce92ae5d465adf838f0b
Amir Dehbashi <a.dehbashi100@gmail.com>
2022-04-07 02:28:42 +0430

    first test done

--- app/src/test/java/com/majazeh/risloo/ExampleUnitTest.java
+++ app/src/test/java/com/majazeh/risloo/ExampleUnitTest.java
@@ -1,17 +1,40 @@
 package com.majazeh.risloo;
 
-import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
-import static org.junit.Assert.*;
+import org.junit.Test;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
+
 public class ExampleUnitTest {
+
+    private final Calculator underTest = new Calculator();
+
     @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+    public void itShuoldAddTwoNumbers() {
+
+        // Given
+        int numberOne = 20;
+        int numberTwo = 30;
+
+        // When
+        int result = underTest.add(numberOne, numberTwo);
+
+        // Then
+        int expectedNumber = 50;
+        assertEquals(expectedNumber, result);
     }
+
+    public static class Calculator {
+
+        public int add(int a, int b) {
+            return a + b;
+        }
+
+    }
+
 }
